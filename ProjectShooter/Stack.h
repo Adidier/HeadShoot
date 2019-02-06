595538d1be7692dd378713e2f14b1ddc68ba2b3f
Adidier <adidier.perez@gmail.com>
2019-02-06 08:45:32 -0600

    Add stack class

--- ProjectShooter/Stack.h
+++ ProjectShooter/Stack.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef STACK_H
+#define STACK_H
 #include "Utilities/DoubleLinkedList.h"
 
 template <class dataType>
@@ -7,8 +8,12 @@ class Stack : public  DoubleLinkedList <dataType>
 public:
 
 	void push(dataType p_data);
-
+	void pop();
+	dataType& top();
+	int count();
 	Stack();
 	~Stack();
 };
 
+#include "Stack.cpp"
+#endif
